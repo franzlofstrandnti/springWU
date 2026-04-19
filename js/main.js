cc6378c1d00ffe60fa433ee9d2d56a9a86a28a3d
franzlofstrandnti <franz.lofstrand@elev.ga.ntig.se>
2026-04-20 00:08:31 +0200

    Inlämning
    V.16 Söndag, färdig. 00:08.

--- js/main.js
+++ js/main.js
@@ -15,3 +15,19 @@ function showMenu(){
     layer.classList.toggle("visible")
 }
 
+let topBtn = document.getElementById("back-to-top");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+topBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
